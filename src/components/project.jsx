1e8a3f37ac0b97a6f01059ef930861eb44250623
Corey Ladovsky <coreyskymusic@gmail.com>
2018-04-01 20:21:32 -0400

    working on project component

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 class Project extends React.Component {
   render () {
-    const { image, liveLink, repoLink } = this.props;
+    const { image, liveLink, repoLink, description, title } = this.props;
+    return(
+      <div>
+        <div>title</div>
+        <img src={image} alt=""/>
+        <div>{description}</div>
+       <a href={liveLink} target="_blank">Live</a>
+       <a href={repoLink} target="_blank">Code</a>
+      </div>
+    );
 
   }
 }
