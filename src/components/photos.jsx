6373c1aa2a9809f291302f2b54f972ffac63fba9
Corey Ladovsky <coreyskymusic@gmail.com>
2018-04-09 15:42:21 -0400

    styled photo carousel

--- src/components/photos.jsx
+++ src/components/photos.jsx
@@ -9,6 +9,7 @@ import photo6 from "../images/pic5.jpg";
 import photo7 from "../images/pic6.jpg";
 import photo8 from "../images/pic7.jpg";
 import photo9 from "../images/pic8.jpg";
+import "./photo.css";
 
 class PhotoSlider extends Component {
   render() {
@@ -22,34 +23,23 @@ class PhotoSlider extends Component {
     return (
       <div>
         <Slider {...settings}>
-          <div>
-            <img src={photo1} alt="" />
-          </div>
-          <div>
-            <img src={photo2} alt="" />
-          </div>
-          <div>
-            <img src={photo3} alt="" />
-          </div>
-          <div>
-            <img src={photo4} alt="" />
-          </div>
-          <div>
-            <img src={photo5} alt="" />
-          </div>
-          <div>
-            <img src={photo6} alt="" />
-          </div>
-          <div>
-            <img src={photo7} alt="" />
-          </div>
-          <div>
-            <img src={photo8} alt="" />
-          </div>
-          <div>
-            <img src={photo9} alt="" />
-          </div>
+          <img src={photo1} alt="" />
 
+          <img src={photo2} alt="" />
+
+          <img src={photo3} alt="" />
+
+          <img src={photo4} alt="" />
+
+          <img src={photo5} alt="" />
+
+          <img src={photo6} alt="" />
+
+          <img src={photo7} alt="" />
+
+          <img src={photo8} alt="" />
+
+          <img src={photo9} alt="" />
         </Slider>
       </div>
     );
