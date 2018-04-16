63e10e19156a19fddf90b62200ea26dfd0a15151
Corey Ladovsky <coreyskymusic@gmail.com>
2018-04-16 14:47:18 -0400

    trying to track down minor space on about page in mobile mode

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import resume from "../assets/resume.pdf";
 import "./nav.css";
+import $ from "jquery";
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -11,10 +12,12 @@ class NavBar extends React.Component {
   }
 
   toggleNav() {
-    
+    $(".nav-container").css("display", "none");
   }
 
   handleSelection(event) {
+    $(".nav-container").css("display", "block");
+
           // this.props.history.push(`/home`);
 
     // if(event.target.innerText) {
@@ -37,8 +40,9 @@ class NavBar extends React.Component {
           <i className="fas fa fa-bars menu-bars" onClick={this.handleSelection}></i>
         </div>
         <div>
+          <div className="nav-container">
+            <div className="close-nav" ><div onClick={this.toggleNav}>x</div></div>
           <ul onClick={this.handleSelection} className="nav-ul">
-            <div className="close-nav" onClick={this.toggleNav}>X</div>
             <li className="about-nav">ABOUT</li>
             <li className="projects-nav">PROJECTS</li>
             <li className="skills-nav">
@@ -46,8 +50,9 @@ class NavBar extends React.Component {
             </li>
             <li className="contact-nav">CONTACT</li>
 
-              <a href={resume} className="resume-link resume-nav" target="_blank">RESUME</a>
+              <a href={resume} className="resume-link-nav resume-nav" target="_blank">RESUME</a>
           </ul>
+        </div>
 
 
 
