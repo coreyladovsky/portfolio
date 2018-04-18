2a8a5e8adfc627771a238187ecbdc9bae154ac84
Corey Ladovsky <coreyskymusic@gmail.com>
2018-04-17 20:59:25 -0400

    added hover effects

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { withRouter } from "react-router-dom";
 import resume from "../assets/resume.pdf";
 import "./nav.css";
@@ -13,29 +13,27 @@ class NavBar extends React.Component {
   }
 
   toggleNav() {
-    if(this.showing) {
+    if (this.showing) {
       // debugger
 
       $(".nav-container").removeClass("slideInLeft");
       $(".nav-container").addClass("slideOutLeft");
-      setTimeout(()=> {
+      setTimeout(() => {
         $(".nav-container").css("display", "none");
-
       }, 500);
     } else {
       $(".nav-container").css("display", "grid");
       $(".nav-container").removeClass("slideOutLeft");
       $(".nav-container").addClass("slideInLeft");
-
     }
     this.showing = !this.showing;
   }
 
   handleSelection(event) {
-    if(event.target.innerText) {
+    if (event.target.className === "li-selector") {
       let nextPage = event.target.innerText.toLowerCase();
-      if(nextPage !== "resume") {
-        if(nextPage === "about") {
+      if (nextPage !== "resume") {
+        if (nextPage === "about") {
           this.props.history.push(`/`);
         } else {
           this.props.history.push(`/${nextPage}`);
@@ -46,33 +44,42 @@ class NavBar extends React.Component {
   }
 
   // <div> MENU </div>
-  render () {
-    return(
+  render() {
+    return (
       <div id="navBar-container">
-        <div className="menu" >
-          <i className="fas fa fa-bars menu-bars" onClick={this.toggleNav}></i>
+        <div className="menu">
+          <i className="fas fa fa-bars menu-bars" onClick={this.toggleNav} />
         </div>
         <div>
           <div className="nav-container">
-            <div className="close-nav" ><div onClick={this.toggleNav}>x</div></div>
-          <ul onClick={this.handleSelection} className="nav-ul">
-            <li className="about-nav">ABOUT</li>
-            <li className="projects-nav">PROJECTS</li>
-            <li className="skills-nav">
-              SKILLS
-            </li>
-            <li className="contact-nav">CONTACT</li>
-
-              <a href={resume} className="resume-link-nav resume-nav" target="_blank">RESUME</a>
-          </ul>
+            <div className="close-nav">
+              <div onClick={this.toggleNav}>x</div>
+            </div>
+            <ul onClick={this.handleSelection} className="nav-ul">
+              <li className="about-nav">
+                <div className="li-selector">ABOUT</div>
+              </li>
+              <li className="projects-nav">
+                <div className="li-selector">PROJECTS</div>
+              </li>
+              <li className="skills-nav">
+                <div className="li-selector">SKILLS</div>
+              </li>
+              <li className="contact-nav">
+                <div className="li-selector">CONTACT</div>
+              </li>
+              <div className="li-selector">
+                <a
+                  href={resume}
+                  className="resume-link-nav resume-nav"
+                  target="_blank"
+                >
+                  RESUME
+                </a>
+              </div>
+            </ul>
+          </div>
         </div>
-
-
-
-        </div>
-
-
-
       </div>
     );
   }
