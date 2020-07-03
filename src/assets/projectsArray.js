1811b3d94331a78b0082b249a12f05ea64dad323
Corey Ladovsky <coreyskymusic@gmail.com>
2020-07-03 17:35:39 -0400

    helper queue, etch a sketch, and projectsArray refactor

---
+++ src/assets/projectsArray.js
@@ -0,0 +1,120 @@
+import help from "../images/help.jpg";
+import mazeSpinner from "../images/maze_spinner.png";
+import NBAStats from "../images/nba_stats.png";
+import MasterDomain from "../images/master_of_the_domain.png";
+import StockStudy from "../images/stock_study.png";
+import Carousel from "../images/carousel.png";
+import Overflow from "../images/overflow.png";
+import tictactoe from "../images/tictactoe.png";
+import WordLadder from "../images/wordladder.png";
+import Connect4 from "../images/connect4.png";
+import Calculator from "../images/calculator.png";
+import EtchASketch from "../images/etchasketch.png";
+import HelperQueue from "../images/helperqueue.png";
+
+export const projectsArray = [
+  {
+    title: "Help",
+    liveLink: "https://www.corey-ladovsky-help.club",
+    repoLink: "https://github.com/coreyladovsky/help",
+    description:  "A single page application in the style of Yelp where users are able to rate and review restaurants. Built with Rails, React, Redux, AWS, CSS3, HTML5, and PostgreSQL",
+    image: help,
+    facon:"icon fa fas fa-utensils"
+  },
+  {
+    title: "Maze Spinner",
+    liveLink: "https://www.coreyladovsky.com/Maze-Spinner/",
+    repoLink: "https://github.com/coreyladovsky/Maze-Spinner",
+    description: "Navigate your way through multiple mazes while they randomly rotate. Built with JavaScript, HTML5, jQuery, and Canvas.",
+    image: mazeSpinner,
+    facon:"icon fa fa-gamepad"
+  },
+  {
+    title: "NBA Stats By The Minute 2017",
+    liveLink: "https://nba-stats-corey-ladovsky.herokuapp.com",
+    repoLink: "https://github.com/coreyladovsky/bball",
+    description: "An NBA data visualization done with d3 inside of a react redux app.",
+    image: NBAStats,
+    facon:"icon fab fa-dribbble"
+  },
+  {
+    title: "Helper Queue",
+    liveLink: "https://helper-queue.netlify.app/",
+    repoLink: "https://github.com/coreyladovsky/helper_queue",
+    description: "A student resource as well as a classroom tool that allows students to login and request help in a queue. This is a fullstack applicaton built with Node, Express, React / React Context, Firebase Authentication, Socket.io, HTML5, CSS3, PostgreSQL, and React Testing Library.",
+    image: HelperQueue,
+    facon:"icon fa fa-bell"
+  },
+  {
+    title: "Master Of The Domain",
+    liveLink: "https://www.coreyladovsky.com/Master-Of-The-Domain/",
+    repoLink: "https://github.com/coreyladovsky/Master-Of-The-Domain",
+    description: "Master Of The Domain is a JavaScript DOM manipulator inspired by the wonderful jQuery. Demonstrated as a snake game.",
+    image: MasterDomain,
+    facon:"icon fa fa-code"
+  },
+  {
+    title: "Stock Study",
+    liveLink: "https://www.coreyladovsky.com/stock_study/",
+    repoLink: "https://github.com/coreyladovsky/stock_study",
+    description: "A d3 data visualization of five stocks and their performace over the past week.",
+    image: StockStudy,
+    facon:"icon fa fas fa-chart-line"
+  },
+  {
+    title: "Carousel",
+    liveLink: "https://www.coreyladovsky.com/Carousel/",
+    repoLink: "https://github.com/coreyladovsky/Carousel",
+    description: "A responsive photo carousel made with React.",
+    image: Carousel,
+    facon:"icon fa fa-camera-retro"
+  },
+  {
+    title: "Overflow",
+    liveLink: "https://www.coreyladovsky.com/overflow-game/",
+    repoLink: "https://github.com/coreyladovsky/overflow-game",
+    description: "An interactive and highly addictive color changing game made with React",
+    image: Overflow,
+    facon:"icon fa fa-gamepad"
+  },
+  {
+    title: "Word Ladder",
+    liveLink: "https://www.coreyladovsky.com/wordLadder/",
+    repoLink: "https://github.com/coreyladovsky/wordLadder",
+    description: "A JavaScript word network that finds the shortest path between two words of equal length changing only one character at a time.",
+    image: WordLadder,
+    facon:"icon fa fa-book"
+  },
+  {
+    title: "Connect 4",
+    liveLink: "https://www.coreyladovsky.com/connect4/",
+    repoLink: "https://github.com/coreyladovsky/connect4",
+    description: "A classic childhood game built with Object Oriented JavaScript",
+    image: Connect4,
+    facon:"icon fa fa-gamepad"
+  },
+  {
+    title: "Calculator",
+    liveLink: "https://www.coreyladovsky.com/calculator/",
+    repoLink: "https://github.com/coreyladovsky/calculator",
+    description: "A React calulator with identical behaivor to the popular Iphone calulator.",
+    image: Calculator,
+    facon:"icon fa fa-calculator"
+  },
+  {
+    title: "Etch A Sketch",
+    liveLink: "https://www.coreyladovsky.com/etchASketch/",
+    repoLink: "https://github.com/coreyladovsky/etchASketch",
+    description: "A retro etch a sketch build with JavaScript and Canvas",
+    image: EtchASketch,
+    facon:"icon fa fa-pencil-alt "
+  },
+  {
+    title: "TickyTacky",
+    liveLink: "https://www.coreyladovsky.com/tickyTacky/",
+    repoLink: "https://github.com/coreyladovsky/tickyTacky",
+    description: "An all JavaScript two player tic tac toe game.",
+    image: tictactoe,
+    facon:"icon fa fa-gamepad "
+  }
+]
